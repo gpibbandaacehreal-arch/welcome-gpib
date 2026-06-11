4945ccdaca2ded59628df6c1282e2720126179fc
gpibbandaacehreal-arch <289879188+gpibbandaacehreal-arch@users.noreply.github.com>
2026-06-11 09:17:33 +0700

    fix: remove unused useEffect and finalize global sync

--- src/components/DownloadProposal.tsx
+++ src/components/DownloadProposal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { generateProposalPDF, type ProposalData } from '../utils/pdfUtils';
 
 interface ProposalRecord extends ProposalData {
